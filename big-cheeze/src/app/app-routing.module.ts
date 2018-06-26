5b58c6dafd3012325424cd6257fd3459a4c3dee4
Cheserae Beukes <cheseraeb@telesure.co.za>
2018-06-26 13:41:12 +0200

    all

---
+++ big-cheeze/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignupComponent } from './signup/signup.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from '../home/home.component';
+// import { EmployeeListComponent } from '../employee-list/employee-list.component';
+// import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
+// import { SignupComponent } from '../signup/signup.component';
+// import { LogInComponent } from '../log-in/log-in.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'employeelist', component: EmployeeListComponent},
+  { path: 'employeedetails', component: EmployeeDetailsComponent},
+  { path: 'login', component: LogInComponent},
+  { path: 'signup', component: SignupComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+export const routingComponents = [HomeComponent, EmployeeListComponent, EmployeeDetailsComponent, LogInComponent, SignupComponent];
--- big-cheeze/src/app/app.module.ts
+++ big-cheeze/src/app/app.module.ts
@@ -1,32 +1,36 @@
-import { HttpClientJsonpModule } from '@angular/common/http';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
-
+// Components
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
-import { LogInComponent } from './log-in/log-in.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EmployeeService } from './employee.service';
+import { AppRoutingModule, routingComponents} from './app-routing.module';
+// import { LogInComponent } from './log-in/log-in.component';
+// import { SignupComponent } from './signup/signup.component';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
+// import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+// import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     NavComponent,
-    LogInComponent,
-    EmployeeListComponent,
-    EmployeeDetailsComponent
+    routingComponents
+    // LogInComponent,
+    // SignupComponent,
+    // EmployeeListComponent,
+    // EmployeeDetailsComponent,
+    // HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
-
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
