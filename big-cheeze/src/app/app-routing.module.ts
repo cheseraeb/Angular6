efd051a6b2248ce565645d6f8d476dbe6fca0143
Cheserae Beukes <cheseraeb@telesure.co.za>
2018-06-27 09:46:29 +0200

    big cheeze

--- big-cheeze/src/app/app-routing.module.ts
+++ big-cheeze/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,11 +7,6 @@ import { SignupComponent } from './signup/signup.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { HomeComponent } from './home/home.component';
-// import { HomeComponent } from '../home/home.component';
-// import { EmployeeListComponent } from '../employee-list/employee-list.component';
-// import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
-// import { SignupComponent } from '../signup/signup.component';
-// import { LogInComponent } from '../log-in/log-in.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
@@ -18,6 +14,7 @@ const routes: Routes = [
   { path: 'employeedetails', component: EmployeeDetailsComponent},
   { path: 'login', component: LogInComponent},
   { path: 'signup', component: SignupComponent},
+  // Wildcard always at bottom
   { path: '**', component: PageNotFoundComponent},
 ];
 
